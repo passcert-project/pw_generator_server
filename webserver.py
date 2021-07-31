0c71dfb6612e8cb4687777b6353cc15aac3c474a
mikibakaiki <joaomiguelcampos6@gmail.com>
2021-07-31 19:32:07 +0100

    updated the server

--- webserver.py
+++ webserver.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import subprocess
+import json
 from flask import Flask, request, jsonify
 
 api = Flask(__name__)
@@ -7,13 +8,15 @@ api = Flask(__name__)
 
 @api.route('/generate', methods=['POST'])
 def generate_random_password():
-    pw_options = request.form.get('options')
+    pw_options = request.get_json()
+    print(f'GOT THESE OPTIONS => {pw_options}')
+    pw_options = pw_options.get('pw_settings')
     x = pw_options.split()
-    # print(x)
 
     result = subprocess.run(["./passwordGeneratorApp.out", "-a", f"{x[0]}", f"{x[1]}", f"{x[2]}", f"{x[3]}", f"{x[4]}",
                             f"{x[5]}", f"{x[6]}", f"{x[7]}", f"{x[8]}"], stdout=subprocess.PIPE).stdout.decode('utf-8')
     result = result.split()
+    print(f'\n\n RESULT => {result}\n\n')
     print(f'\n\nGenerated Password => {result[2]}\n\n')
 
     return jsonify({"generated_password": result[2]})
