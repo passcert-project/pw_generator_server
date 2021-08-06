81c365285e75309154dcc141a70a9c940e4be396
mikibakaiki <joaomiguelcampos6@gmail.com>
2021-08-06 21:53:00 +0100

    small changes

--- generate_bw_passwords.py
+++ generate_bw_passwords.py
@@ -10,15 +10,6 @@ start = time.time()
 for i in range(int(sys.argv[1])):
     result = subprocess.run(
         ["bw", "generate"], stdout=subprocess.PIPE).stdout.decode('utf-8')
-
-    # if (int(sys.argv[1])/(i+1) >= int(sys.argv[1])/4):
-    #     print(f'25% Done')
-    # elif (int(sys.argv[1])/(i+1) >= int(sys.argv[1])/2):
-    #     print(f'50% Done')
-    # elif (int(sys.argv[1])/(i+1) >= (3*int(sys.argv[1]))/4):
-    #     print(f'75% Done')
-    # elif (int(sys.argv[1])/(i+1) == int(sys.argv[1])):
-    #     print(f'100% Done')
     textfile = open(f"{sys.argv[2]}", "a")
     a = textfile.write(f'{result}\n')
     textfile.close()
